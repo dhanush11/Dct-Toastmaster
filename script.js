b9706d5f4827e894f318e5d52e84beada4194487
Dhanush <dhanush.11ror@gmail.com>
2018-09-24 13:23:03 +0530

    changed data fetching

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ var rotateHandle = document.getElementById('rotate');
 var podiumWheel;
 var result = [];
 var parts = [];
+var batches;
 var style =['#FF3B58', '#79D3B1', '#FEF027', '#E94E77', '#3A8ECF', '#F7752C', '#EB007F', '#DE000F', '#31AE88','#33E46D','#CDFC43','#FF5039','#eae56f','#89f26e']
 
 window.addEventListener('load', function(){
   axios.get('http://localhost:3000/batches').then((response) => {
-    let batches = response.data;
+    batches = response.data;
     batches.forEach(function(batch){
         var optionHandle = document.createElement('option');
         var optionHandleText = document.createTextNode(batch.name);
@@ -20,35 +21,54 @@ window.addEventListener('load', function(){
 })
 
 selectHandle.addEventListener('change', function(){
-  result = [];
   parts = [];
-  axios.get(`http://localhost:3000/batches/${selectHandle.value}`).then((response) => {
-    let batch = response.data;
-  batch.students.forEach((student) => {
-    if(selectHandle.value == student.batches[student.batches.length-1]){
-      result.push(student.name);
-    }
+//  result = [];
+//   axios.get(`http://localhost:3000/batches/${selectHandle.value}`).then((response) => {
+//     let batch = response.data;
+//   batch.students.forEach((student) => {
+//     if(selectHandle.value == student.batches[student.batches.length-1]){
+//       result.push(student.name);
+//     }
+//   })
+//
+//   result.forEach(function(result, i){
+//     parts.push({'fillStyle' : style[i], 'text' : result})
+//   })
+//
+//   podiumWheel = new Winwheel({
+//     'numSegments' : result.length,
+//     'outerRadius' : 170,
+//     'segments' : parts,
+//     'animation' : {
+//       'type': 'spinToStop',
+//       'duration': 10,
+//       'callbackFinished': 'message()',
+//       'callbackAfter' : 'targetIndicator()'
+//     }
+//   });
+// }).catch((err) => {
+//   console.log(err);
+// })
+// })
+  var batch = batches.find(batch => selectHandle.value == batch._id )
+  batch.students.forEach((student, i) =>{
+    parts.push({'fillStyle' : style[i], 'text' : student.name})
   })
 
-  result.forEach(function(result, i){
-    parts.push({'fillStyle' : style[i], 'text' : result})
-  })
+    podiumWheel = new Winwheel({
+      'numSegments' : parts.length,
+      'outerRadius' : 170,
+      'segments' : parts,
+      'animation' : {
+        'type': 'spinToStop',
+        'duration': 10,
+        'callbackFinished': 'message()',
+        'callbackAfter' : 'targetIndicator()'
+      }
+    })
+})
 
-  podiumWheel = new Winwheel({
-    'numSegments' : result.length,
-    'outerRadius' : 170,
-    'segments' : parts,
-    'animation' : {
-      'type': 'spinToStop',
-      'duration': 10,
-      'callbackFinished': 'message()',
-      'callbackAfter' : 'targetIndicator()'
-    }
-  });
-}).catch((err) => {
-  console.log(err);
-})
-})
+
 
 function message(){
   var selectedPerson = podiumWheel.getIndicatedSegment();
